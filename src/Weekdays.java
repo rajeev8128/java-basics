5993b1bcc15700f1a25e1f0650848711f7683ca5
rajeev8128 <rajendra.rajeev.singh@gmail.com>
2024-09-08 14:49:25 +0530

    Updated

--- src/Weekdays.java
+++ src/Weekdays.java
@@ -1,12 +1,25 @@
+import java.io.PrintStream;
+import java.util.Scanner;
+
 public class Weekdays {
 
     // Create a class which shows weekday on user inputs by using arrays
 
     public static void main(String[] args) {
 
-        String [] Weekdays = {"Monday","Tuesday","Wednesday","Thursday","Friday","Satur"}
-    }
+        String[] Weekdays_name = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
 
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the day number : ");
+        int Userinput = in.nextInt();
+        for (int i = 0; i < 7; i++) {
+            if (i + 1 == Userinput) {
+                System.out.println("Coresponding day name is : "+Weekdays_name[i]);
+            }
+
+        }
+    }
 }
+
 
 
